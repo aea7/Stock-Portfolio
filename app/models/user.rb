4100ba40d50970caa7df302989f0adf4615e4163
Abdullah Ak <aeak1994@gmail.com>
2016-06-23 09:48:38 +0000

    y

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,11 @@ class User < ActiveRecord::Base
       under_stock_limit? && !stock_already_added?(ticker_symbol)
     end
   
+  def full_name
+	  return "#{first_name} #{last_name}".strip if (first_name || last_name)
+	  "Anonymous"
+  end
+  
   def under_stock_limit?
     (user_stocks.count < 10)
   end
